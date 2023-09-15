07f29ad4b44c99915d4c3dab521c7b8692b36f1b
Ashutosh Kesarwani <ashutoshkesarwani999@gmail.com>
2023-09-15 23:45:27 +0530

    Error Handling

--- index.ts
+++ index.ts
@@ -15,11 +15,18 @@ console.log("lgging")
 console.log("Asynchronous/Non-Blocking Code")
 //Asynchronous Code or Non-Blocking Code
 fs.readFile('./txt/start.txt',(err,data1)=>{
+    if(err) return console.log("ERROR: ",err)
     fs.readFile(`./txt/${data1.toString()}.txt`,(err2,data2)=>{
+        if(err) return console.log("ERROR: ",err)
+
         console.log(data2.toString())
         fs.readFile('./txt/append.txt',(err3,data3)=>{
+            if(err) return console.log("ERROR: ",err)
+
             console.log(data3.toString())
             fs.writeFile('./txt/final.txt',`${data2.toString()}\n${data3.toString()}`,'utf-8',err=>{
+                if(err) return console.log("ERROR: ",err)
+
                 console.log('Your file has been written')
             })
         })
