11b977335084cde6e2a21bee1b2c803c8308cc6f
Ashutosh Kesarwani <ashutoshkesarwani999@gmail.com>
2023-09-20 13:40:24 +0530

    Add a simple API

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import http from 'http';
+import path from 'path';
 
 //synchronous code or Blocking code
 
@@ -37,25 +38,42 @@ import http from 'http';
 
 // SERVER
 
-const server = http.createServer((req,res)=>{
-    console.log("Request Object",req.url)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data)
+
+const server = http.createServer((req, res) => {
+    console.log("Request Object", req.url)
     const pathName = req.url
-    if(pathName =='/overview' || pathName == '/'){
-        res.end('Hello from the server!')        
+    if (pathName == '/overview' || pathName == '/') {
+        res.end('Hello from the server!')
     }
-    else if(pathName == '/product'){
+    else if (pathName == '/product') {
         res.end('This is Product')
     }
-    else{
-        res.writeHead(404,{
-            'Content-type':'text/html', // represents the content displayed on browser is HTML hence res.end has html tag attached
-            'my-own-header':'hello-world' // our own header to explain the error if we want to
+    else if (pathName == '/api') {
+
+        // This will re read the data from JSON file everytime we hit the /api 
+    //     fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => { 
+    //         const productData = JSON.parse(data) 
+    //         console.log(typeof data)
+
+    //         res.writeHead(200,{'Content-type':'application/json'});
+    //         res.end(data);
+    // })
+        res.writeHead(200,{'Content-type':'application/json'});
+        res.end(data);
+
+        // res.end('API')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html', // represents the content displayed on browser is HTML hence res.end has html tag attached
+            'my-own-header': 'hello-world' // our own header to explain the error if we want to
         });
         res.end('<h1>Page not found!</h1>')
     }
 
 })
- 
-server.listen(8000,'127.0.0.1',()=>{
+
+server.listen(8000, '127.0.0.1', () => {
     console.log("Listening to port 8000")
 })
